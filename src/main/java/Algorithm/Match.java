da491dc3623ad75edf0a25e09d31d3ad6f0ae986
Mohamed Ayman <mohamed16130@stemegypt.edu.eg>
2022-05-11 18:31:13 +0200

    Modified Match.java added enums for rounds and match categories added implementation for calcImportance added calcImmunity method modified constructor, some variables to use the enums

--- src/main/java/Algorithm/Match.java
+++ src/main/java/Algorithm/Match.java
@@ -9,34 +9,65 @@ public class Match {
     private Pair<Double, Double> wins;
     private Pair<Double, Double> expectedWins;
     private int importance;
-    private String competitionType;
-    private String round;
+    private Categories competitionType;
+    private Rounds round;
     private Boolean PSO;
 
 
-    public Match(Team team1, Team team2, String competitionType, String round,
-                 int team1Score, int team2Score, int importance, Boolean PSO) {
+    public Match(Team team1, Team team2, Categories competitionType, Rounds round,
+                 int team1Score, int team2Score, int importance, Boolean PSO,Boolean inCalendar) {
         this.team1 = team1;
         this.team2 = team2;
         this.competitionType = competitionType;
         this.round = round;
         this.PSO = PSO;
-        this.importance = calcImportance(competitionType, round);
-        wins = calculateWins(team1Score, team2Score);
+        this.importance = calcImportance(competitionType, round, inCalendar);
+        wins = calculateWins(team1Score, team2Score,PSO);
         expectedWins = calculateExpectedWins();
-        Boolean knockOutRounds = false;
-        if (this.round != "Group Stage"){
-            knockOutRounds = true;
+        Boolean immunity = calcImmunity(competitionType,round);         //true if a match is in the knockout stages of a final competition
+        if (this.round != Rounds.Group_Stage){
+            immunity = true;
         }
-        team1.calculateNewPoints(importance,wins.getKey(),expectedWins.getKey(), knockOutRounds);
-        team2.calculateNewPoints(importance,wins.getValue(),expectedWins.getValue(),knockOutRounds);
+        team1.calculateNewPoints(importance,wins.getKey(),expectedWins.getKey(), immunity);
+        team2.calculateNewPoints(importance,wins.getValue(),expectedWins.getValue(),immunity);
 
 
     }
-    public int calcImportance(String competitionType, String round){
-        // add if conditions to calculate importance
-        return 1;
-
+    public int calcImportance(Categories competitionType, Rounds round,Boolean inCalendar){
+        int Importance;
+        switch(competitionType){
+            case Friendly_Match:
+                if(inCalendar){
+                    return 10;
+                }else{
+                    return 5;
+                }
+            case Nations_League:
+                if(round == Rounds.Group_Stage){
+                    return 15;
+                }else{
+                    return 25;
+                }
+            case Q_for_Conf:
+            case Q_for_World_Cup:
+                return 25;
+            case Confederation_Final:
+                if(round == Rounds.Group_Stage || round == Rounds.Round_of_16){
+                    return 35;
+                }else{
+                    return 40;
+                }
+            case Confederations_Cup:
+                return 40;
+            case World_Cup:
+                if(round == Rounds.Group_Stage || round == Rounds.Round_of_16){
+                    return 50;
+                }else{
+                    return 60;
+                }
+            default:
+                return 0;       // if fore some reason the competition type isn't from the list
+        }
     }
     public Pair<Double, Double> calculateExpectedWins(){
         double drTeam1 = calcRatingDifference(team1.getPoints(), team2.getPoints());
@@ -54,20 +85,28 @@ public class Match {
         return (team1Points - team2Points);
     }
 
-    public Pair<Double, Double> calculateWins(int team1Score, int team2Score){
-        if (team1Score > team2Score){
-            if (this.PSO){
+    public Pair<Double, Double> calculateWins(int team1Score, int team2Score,boolean PSO){
+        if (team1Score > team2Score){                       //this function needs to be modified Todo
+            if (PSO){
                 return new Pair<>(0.75, 0.5);
             }
             return new Pair<>(1.0, 0.0);
         }
         else if (team1Score < team2Score) {
-            if (this.PSO){
+            if (PSO){
                 return new Pair<>(0.5, 0.75);
             }
             return new Pair<>(0.0, 1.0);
         }
         return new Pair<>(0.5, 0.5);
+    }
+    public boolean calcImmunity(Categories competitionType, Rounds round){
+        if(round == Rounds.Group_Stage)return false;
+        if(competitionType==Categories.Confederation_Final||competitionType==Categories.Confederations_Cup||competitionType==Categories.World_Cup){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public Team getTeam1() {
@@ -118,19 +157,54 @@ public class Match {
         this.expectedWins = expectedWins;
     }
 
-    public String getCompetitionType() {
+    public Categories getCompetitionType() {
         return competitionType;
     }
 
-    public void setCompetitionType(String competitionType) {
+    public void setCompetitionType(Categories competitionType) {
         this.competitionType = competitionType;
     }
 
-    public String getRound() {
+    public Rounds getRound() {
         return round;
     }
 
-    public void setRound(String round) {
+    public void setRound(Rounds round) {
         this.round = round;
     }
+
+    public enum Rounds {
+        Group_Stage("Group Stage"),
+        Round_of_16("Round of 16"),
+        Quarter_Final("Quarter Final"),
+        Semi_Final("Semi Final"),
+        Final("Final");
+
+        public final String label;
+
+        private Rounds(String label) {
+            this.label = label;
+        }
+        public String toString() {          // check if you need this Todo
+            return label;
+        }
+    }
+    public static enum Categories{
+        Friendly_Match("Friendly Match"),
+        Nations_League("Nations League"),
+        Q_for_Conf("Qualifier for Confederation final competition"),
+        Q_for_World_Cup("Qualifier for World Cup"),
+        Confederation_Final("Confederation final competition"),
+        Confederations_Cup("Confederations Cup"),
+        World_Cup("World Cup");
+        public final String label;
+        private Categories(String label) {
+            this.label = label;
+        }
+        public String toString() {          // check if you need this Todo
+            return label;
+        }
+
+    }
+    //String r = Round.Final.label;
 }
