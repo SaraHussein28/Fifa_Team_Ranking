a92f991d9620966c30d25f183775f6633ce85aba
Mohamed Ayman <mohamed16130@stemegypt.edu.eg>
2022-05-30 17:35:59 +0200

    updated gui to use objects andenums instead of strings

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.demo1;
 
+import Algorithms.Match;
+import Algorithms.Team;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -9,19 +11,19 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import util.ViewUtil;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.time.LocalDate;
 
 public class HelloController {
 
-    ObservableList <String> listOfTeams = FXCollections.observableArrayList("Egypt", "Italy", "England");
-    ObservableList <String> listOfTeams2 = FXCollections.observableArrayList("egypt", "italy", "england");
 
-    ObservableList <String> teams = FXCollections.observableArrayList("team 1", "team 2");
-    ObservableList <String> listOfRounds = FXCollections.observableArrayList("Group Stage","Round of 16", "Quarter-finals", "Semi-finals", "Final");
-    ObservableList <String> listOfCategories = FXCollections.observableArrayList("Friendly match", "Nations League", "Qualifier for Confederation final competition", "Qualifier for World Cup", "Confederation final competition", "Confederations Cup", "World Cup");
+    ObservableList <Team> listOfTeams;// = FXCollections.observableArrayList("Egypt", "Italy", "England");
+    //ObservableList <String> teams = FXCollections.observableArrayList("Team 1", "Team 2");
+    ObservableList <Team> selectedTeams = FXCollections.observableArrayList();
+    //ObservableList <String> listOfRounds = FXCollections.observableArrayList("Group Stage","Round of 16", "Quarter-finals", "Semi-finals", "Final");
+    //ObservableList <String> listOfCategories = FXCollections.observableArrayList("Friendly match", "Nations League", "Qualifier for Confederation final competition", "Qualifier for World Cup", "Confederation final competition", "Confederations Cup", "World Cup");
 
     @FXML
     private Button showFIFARankingBtn;
@@ -44,10 +46,10 @@ public class HelloController {
     private Label choose_winner_txt;
 
     @FXML
-    private TextField score_team_one;
+    private Label score_team1;
 
     @FXML
-    private TextField score_team_two;
+    private Label score_team2;
     @FXML
     private ChoiceBox winner_choice_box;
     @FXML
@@ -56,28 +58,39 @@ public class HelloController {
     @FXML
     protected void initialize(){
 
+        try {
+            Team.GetAllTeams();
+            //System.out.println(Team.teams_list);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        listOfTeams = Team.teams_list;
+
         initializeInputValues();
     }
 
     private void initializeInputValues() {
 
 
-        team1_choice_box.setValue("Egypt");
+        //team1_choice_box.setValue("Egypt");
         team1_choice_box.setItems(listOfTeams);
+        team1_choice_box.setValue(listOfTeams.get(0));
 
-
-        team2_choice_box.setValue("england");
-        team2_choice_box.setItems(listOfTeams2);
+        //team2_choice_box.setValue("England");
+        team2_choice_box.setItems(listOfTeams);
+        team2_choice_box.setValue(listOfTeams.get(1));
 
         round_choice_box.setValue("Group Stage");
-        round_choice_box.setItems(listOfRounds);
+        round_choice_box.getItems().setAll(Match.Rounds.values());
 
 
-        category_choice_box.setValue("Friendly match");
-        category_choice_box.setItems(listOfCategories);
+        category_choice_box.setValue(Match.Categories.Friendly_Match);
+        category_choice_box.getItems().setAll(Match.Categories.values());
 
-        winner_choice_box.setValue("team 1");
-        winner_choice_box.setItems(teams);
+        //winner_choice_box.setValue("Team 1");
+        //winner_choice_box.setItems(teams);
+        //winner_choice_box.setValue(selectedTeams.get(0));
+        //winner_choice_box.getItems().setAll(selectedTeams);
         choose_winner_txt.setVisible(false);
         winner_choice_box.setVisible(false);
 
@@ -86,7 +99,7 @@ public class HelloController {
 
     @FXML
     protected void onCategoryDragDone(){
-        if (category_choice_box.getValue().equals("Friendly match")){
+        if (category_choice_box.getValue().equals(Match.Categories.Friendly_Match)){
             calender_check_box.setDisable(false);
             round_choice_box.setDisable(true);
         }
@@ -98,10 +111,16 @@ public class HelloController {
 
     }
     @FXML
-    protected void onAddMatchButtonClick(){
-
-        /*
+    protected void updateSelectedTeams(){
+        selectedTeams.setAll((Team) team1_choice_box.getValue(),(Team) team2_choice_box.getValue());
+        //System.out.println(selectedTeams);
+        winner_choice_box.getItems().setAll(selectedTeams);
+        winner_choice_box.setValue(selectedTeams.get(0));
+    }
+    @FXML
+    protected void onAddMatchButtonClick() throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("popupWindow.fxml"));
+
         Scene scene = new Scene(root);
         Stage primaryStage = new Stage();
         primaryStage.setTitle("Match is added");
@@ -109,23 +128,7 @@ public class HelloController {
         primaryStage.initModality(Modality.WINDOW_MODAL);
         primaryStage.initOwner(addMatchBtn.getScene().getWindow());
         primaryStage.show();
-*/
-        String team1 = team1_choice_box.getValue().toString().toLowerCase();
-        String team2 = team2_choice_box.getValue().toString().toLowerCase();
-
-        if (team1.equals(team2)){
-            ViewUtil.createAlert(Alert.AlertType.ERROR, "The two teams should be different").showAndWait();
-
-        }
-        else if (score_team_one.getText().isEmpty()  || score_team_two.getText().isEmpty()){
-           ViewUtil.createAlert(Alert.AlertType.ERROR, "Please enter all the fields").showAndWait();
-       }
-        else{
-           ViewUtil.createAlert(Alert.AlertType.INFORMATION, "Match is added").showAndWait();
-
-       }
     }
-
 
     @FXML
     protected void OnShootoutSelected (){
