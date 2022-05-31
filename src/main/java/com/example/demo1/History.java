b49160c06aa198d8dde4241bce8318392cbe4c92
Hanya Adel <haniajed79@gmail.com>
2022-05-31 23:07:51 +0200

    Added rounding to double values

--- src/main/java/com/example/demo1/History.java
+++ src/main/java/com/example/demo1/History.java
@@ -6,18 +6,10 @@ import javafx.collections.ObservableList;
 
 
 import java.sql.SQLException;
-import java.util.Collections;
 import java.util.Comparator;
 
 import static java.lang.Integer.parseInt;
 
-//class StudentComparator implements Comparator<History> {
-//    @Override
-//    public int compare(History h1, History h2) {
-//        return h1.points > h2.points;
-//    }
-//}
-//
 
 public class History {
     private int rank;
@@ -26,39 +18,26 @@ public class History {
 
 
     private static int changeToInt(String month){
-        switch (month){
-            case "January":
-                return 1;
-            case "February":
-                return 2;
-            case "March":
-                return 3;
-            case "April":
-                return 4;
-            case "May":
-                return 5;
-            case "June":
-                return 6;
-            case "July":
-                return 7;
-            case "August":
-                return 8;
-            case "September":
-                return 9;
-            case "October":
-                return 10;
-            case "November":
-                return 11;
-            case "December":
-                return 12;
-            default:
-                return 0;
-        }
+        return switch (month) {
+            case "January" -> 1;
+            case "February" -> 2;
+            case "March" -> 3;
+            case "April" -> 4;
+            case "May" -> 5;
+            case "June" -> 6;
+            case "July" -> 7;
+            case "August" -> 8;
+            case "September" -> 9;
+            case "October" -> 10;
+            case "November" -> 11;
+            case "December" -> 12;
+            default -> 0;
+        };
     }
 
 
 
-    public static ObservableList<History> getHistory(String date) throws SQLException, InterruptedException {
+    public static ObservableList<History> getHistory(String date) throws SQLException{
 
         String[] tokens=date.split("-");
 
@@ -80,14 +59,14 @@ public class History {
             double p = Team.getScoreFromHistory(t.getName(), month, year);
 
 
-            h.points = p;
+            h.points = Math.round(p*100.0)/100.0;
             h.teamName = t.getName();
 
             list.add(h);
 
         }
 
-        Collections.sort(list, new Comparator<History>() {
+        list.sort(new Comparator<History>() {
             @Override
             public int compare(History c1, History c2) {
                 return Double.compare(c2.points, c1.points);
@@ -102,10 +81,7 @@ public class History {
         }
         return list;
     }
-    public static void main(String args[]) throws SQLException, InterruptedException {
-        //test_addMatch();
-        //test_getLastMatch();
-        //test_updateScore();
+    public static void main(String args[]) throws SQLException {
         getHistory("May-2022");
     }
 
