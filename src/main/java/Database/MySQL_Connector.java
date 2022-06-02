4f3bc8ed8a991f5f0db107ac128a2951d0560724
Mohamed Ayman <mohamed16130@stemegypt.edu.eg>
2022-06-02 18:05:48 +0200

    updated sql connector to use the online database

--- src/main/java/Database/MySQL_Connector.java
+++ src/main/java/Database/MySQL_Connector.java
@@ -5,8 +5,8 @@ import java.sql.*;
 
 public class MySQL_Connector {
 
-    //private static final String db_url = "jdbc:mysql://sql11.freesqldatabase.com:3306/sql11493736";
-    private static final String db_url = "jdbc:mysql://localhost:3306/sql11493736";
+    private static final String db_url = "jdbc:mysql://sql11.freesqldatabase.com:3306/sql11493736";
+    //private static final String db_url = "jdbc:mysql://localhost:3306/sql11493736";
     private static final String db_username = "sql11493736";
     private static final String db_password = "mymSDXZXIK";
 
