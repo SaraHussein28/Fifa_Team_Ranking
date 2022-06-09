88a0a9acbba61376cc7a9068ee6a83b05641a6b5
Sara Hussein <85261712+SaraHussein28@users.noreply.github.com>
2022-06-09 21:51:33 +0200

    Renamed some classes

--- src/main/java/com/example/demo1/TeamRanking.java
+++ src/main/java/com/example/demo1/TeamRanking.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class TeamRanking extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("team-ranking-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainWindowApplication.class.getResource("team-ranking-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 700);
         stage.setTitle("Fifa Team Ranking");
         stage.setScene(scene);
