a1f3a05a096d7de876d09875928cf20d628365cb
Mohamed Ayman <mohamed16130@stemegypt.edu.eg>
2022-06-11 20:09:04 +0200

    updated main window title

--- src/main/java/com/example/demo1/MainWindowApplication.java
+++ src/main/java/com/example/demo1/MainWindowApplication.java
@@ -12,7 +12,7 @@ public class MainWindowApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainWindowApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 700);
-        stage.setTitle("Fifa Team Ranking");
+        stage.setTitle("FIFA Team Ranking");
         stage.setScene(scene);
         stage.show();
 
@@ -20,6 +20,6 @@ public class MainWindowApplication extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
